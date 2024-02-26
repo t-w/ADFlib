920b89831a338259f617b96b2148d24b85cd11b1
Didier Malenfant <didier@malenfant.net>
2024-02-26 20:49:26 +0100

    Fixed ‘Assigned value is garbage or undefined’
    Testing for the two sums being equal is not the same thing as testing for each separate values being equal. This could lead to some values being un-initialized but also undetected by that test.

--- src/adf_file_block.c
+++ src/adf_file_block.c
@@ -83,8 +83,10 @@ RETCODE adfGetFileBlocks ( struct AdfVolume * const                vol,
             fileBlocks->data[n++] = extBlock.dataBlocks[ ADF_MAX_DATABLK - 1 - i ];
         nSect = extBlock.extension;
     }
-    if ( (fileBlocks->nbExtens+fileBlocks->nbData) != (n+m) )
-        (*adfEnv.wFct)("adfGetFileBlocks : less blocks than expected");
+    if ( (n != fileBlocks->nbData) || (m != fileBlocks->nbExtens) ) {
+        (*adfEnv.wFct)("adfGetFileBlocks : not as many blocks as expected");
+        return RC_ERROR;
+    }
 
     return RC_OK;
 }
