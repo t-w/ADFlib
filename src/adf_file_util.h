d207771ed5c1a49d44b5e6b8dd4f3d6a2812e0a8
t-m <tomas.wolak@gmail.com>
2025-03-11 17:35:11 +0100

    adf_file_util: make const things const.
    Also, some whitespace changes (no other semantic changes).

--- src/adf_file_util.h
+++ src/adf_file_util.h
@@ -16,43 +16,43 @@ ADF_PREFIX int32_t adfFilePos2DataBlock( const unsigned    pos,
                                          unsigned * const  posInDataBlk,
                                          unsigned * const  curDataN );
 
-static inline unsigned adfFilePos2datablockIndex ( unsigned pos,
-                                                   unsigned blocksize )
+static inline unsigned adfFilePos2datablockIndex( const unsigned  pos,
+                                                  const unsigned  blocksize )
 {
-    assert ( blocksize > 0 );
+    assert( blocksize > 0 );
     return ( pos / blocksize );
 }
 
-static inline unsigned adfFileSize2Datablocks ( unsigned fsize,
-                                                unsigned blocksize )
+static inline unsigned adfFileSize2Datablocks( const unsigned  fsize,
+                                               const unsigned  blocksize )
 {
-    assert ( blocksize > 0 );
+    assert( blocksize > 0 );
     return ( fsize / blocksize +
              ( ( fsize % blocksize > 0 ) ? 1 : 0 ) );
 }
 
-static inline unsigned adfFileDatablocks2Extblocks ( unsigned ndatablocks )
+static inline unsigned adfFileDatablocks2Extblocks( const unsigned  ndatablocks )
 {
     //return max ( ( ndata_blocks - 1 ) / ADF_MAX_DATABLK, 0 );
     if ( ndatablocks < 1 )
         return 0;
-    return ( ndatablocks - 1 ) / ( ADF_MAX_DATABLK );
+    return ( ndatablocks - 1 ) / ADF_MAX_DATABLK;
 }
 
-static inline unsigned adfFileSize2Extblocks ( unsigned fsize,
-                                               unsigned blocksize )
+static inline unsigned adfFileSize2Extblocks( const unsigned   fsize,
+                                              const  unsigned  blocksize )
 {
-    return adfFileDatablocks2Extblocks (
-        adfFileSize2Datablocks ( fsize, blocksize ) );
+    return adfFileDatablocks2Extblocks(
+        adfFileSize2Datablocks( fsize, blocksize ) );
 }
 
 
-static inline unsigned adfFileSize2Blocks ( unsigned fsize,
-                                            unsigned blocksize )
+static inline unsigned adfFileSize2Blocks( const unsigned  fsize,
+                                           const unsigned  blocksize )
 {
     //assert ( blocksize > 0 );
-    unsigned data_blocks = adfFileSize2Datablocks ( fsize, blocksize );
-    unsigned ext_blocks  = adfFileDatablocks2Extblocks ( data_blocks );
+    const unsigned data_blocks = adfFileSize2Datablocks( fsize, blocksize );
+    const unsigned ext_blocks  = adfFileDatablocks2Extblocks( data_blocks );
     return data_blocks + ext_blocks + 1;   // +1 for the file header block
 }
 
