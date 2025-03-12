220204bcdfad2ae424e1dca28400c7aebe16f395
t-m <tomas.wolak@gmail.com>
2025-03-12 13:43:37 +0100

    adf_dev_flop: whitespaces (no semantic changes).

--- src/adf_dev_flop.c
+++ src/adf_dev_flop.c
@@ -44,68 +44,68 @@
  * use dev->devType to choose between DD and HD
  * fills the volume list with one volume
  */
-ADF_RETCODE adfMountFlop ( struct AdfDevice * const dev )
+ADF_RETCODE adfMountFlop( struct AdfDevice * const  dev )
 {
     struct AdfVolume *vol;
 
-    vol = (struct AdfVolume *) malloc (sizeof(struct AdfVolume));
-    if (!vol) { 
+    vol = (struct AdfVolume *) malloc( sizeof(struct AdfVolume) );
+    if ( ! vol ) {
         (*adfEnv.eFct)("adfMount : malloc");
         return ADF_RC_MALLOC;
     }
 
     vol->firstBlock = 0;
-    vol->lastBlock = (int32_t) ( dev->cylinders * dev->heads * dev->sectors - 1 );
-    vol->blockSize = 512;
-    vol->dev = dev;
-    vol->volName = NULL;
-    vol->mounted = false;
+    vol->lastBlock  = (int32_t) ( dev->cylinders * dev->heads * dev->sectors - 1 );
+    vol->blockSize  = 512;
+    vol->dev        = dev;
+    vol->volName    = NULL;
+    vol->mounted    = false;
 
     /* set filesystem info (read from bootblock) */
     struct AdfBootBlock boot;
-    ADF_RETCODE rc = adfDevReadBlock (
-        dev, (uint32_t)vol->firstBlock, 512, (uint8_t *)&boot );
+    ADF_RETCODE rc = adfDevReadBlock(
+        dev, (uint32_t) vol->firstBlock, 512, (uint8_t *) &boot );
     if ( rc != ADF_RC_OK ) {
-        adfEnv.eFct ( "adfMountFlop : error reading BootBlock, device %s, volume %d",
-                      dev->name, 0 );
-        free ( vol );
+        adfEnv.eFct( "adfMountFlop : error reading BootBlock, device %s, volume %d",
+                     dev->name, 0 );
+        free( vol );
         return rc;
     }
-    memcpy ( vol->fs.id, boot.dosType, 3 );
-    vol->fs.id[3] = '\0';
-    vol->fs.type = (uint8_t) boot.dosType[3];
+    memcpy( vol->fs.id, boot.dosType, 3 );
+    vol->fs.id[ 3 ] = '\0';
+    vol->fs.type = (uint8_t) boot.dosType[ 3 ];
 
-    if ( adfVolIsDosFS ( vol ) ) {
-        vol->datablockSize = adfVolIsOFS ( vol ) ? 488 : 512;
+    if ( adfVolIsDosFS( vol ) ) {
+        vol->datablockSize = adfVolIsOFS( vol ) ? 488 : 512;
 
-        vol->rootBlock = adfVolCalcRootBlk ( vol );
+        vol->rootBlock = adfVolCalcRootBlk( vol );
         struct AdfRootBlock root;
         vol->mounted = true;    // must be set to read the root block
-        rc = adfReadRootBlock ( vol, (uint32_t) vol->rootBlock, &root );
+        rc = adfReadRootBlock( vol, (uint32_t) vol->rootBlock, &root );
         vol->mounted = false;
         if ( rc != ADF_RC_OK ) {
-            free ( vol );
+            free( vol );
             return rc;
         }
 
-        vol->volName = strndup ( root.diskName,
-                                 min ( root.nameLen,
-                                       (unsigned) ADF_MAX_NAME_LEN ) );
+        vol->volName = strndup( root.diskName,
+                                min( root.nameLen,
+                                     (unsigned) ADF_MAX_NAME_LEN ) );
     } else { // if ( adfVolIsPFS ( vol ) ) {
         vol->datablockSize = 0; //512;
-        vol->volName = NULL;
-        vol->rootBlock = -1;
+        vol->volName       = NULL;
+        vol->rootBlock     = -1;
     }
 
-    dev->volList = (struct AdfVolume **) malloc (sizeof(struct AdfVolume *));
-    if (!dev->volList) {
-        free(vol->volName);
-        free(vol);
-        adfEnv.eFct ( "adfMountFlop : malloc, device '%s'", dev->name );
+    dev->volList = (struct AdfVolume **) malloc( sizeof(struct AdfVolume *) );
+    if ( ! dev->volList ) {
+        free( vol->volName );
+        free( vol );
+        adfEnv.eFct( "adfMountFlop : malloc, device '%s'", dev->name );
         return ADF_RC_MALLOC;
     }
-    dev->volList[0] = vol;
-    dev->nVol = 1;
+    dev->volList[ 0 ] = vol;
+    dev->nVol         = 1;
 
 /*printf("root=%d\n",vol->rootBlock);	    */
     return ADF_RC_OK;
@@ -118,11 +118,11 @@ ADF_RETCODE adfMountFlop ( struct AdfDevice * const dev )
  * create a filesystem on a floppy device
  * fills dev->volList[]
  */
-ADF_RETCODE adfCreateFlop ( struct AdfDevice * const dev,
-                            const char * const       volName,
-                            const uint8_t            volType )
+ADF_RETCODE adfCreateFlop( struct AdfDevice * const  dev,
+                           const char * const        volName,
+                           const uint8_t             volType )
 {
-    if (dev==NULL) {
+    if ( dev == NULL ) {
         (*adfEnv.eFct)("adfCreateFlop : dev==NULL");
         return ADF_RC_NULLPTR;
     }
@@ -130,19 +130,19 @@ ADF_RETCODE adfCreateFlop ( struct AdfDevice * const dev,
         (*adfEnv.eFct)("adfCreateFlop : volName == NULL");
         return ADF_RC_NULLPTR;
     }
-    dev->volList = (struct AdfVolume **) malloc (sizeof(struct AdfVolume *));
-    if (!dev->volList) { 
+    dev->volList = (struct AdfVolume **) malloc( sizeof(struct AdfVolume *) );
+    if ( ! dev->volList ) {
         (*adfEnv.eFct)("adfCreateFlop : malloc");
         return ADF_RC_MALLOC;
     }
-    dev->volList[0] = adfVolCreate ( dev, 0L, 80L, volName, volType );
-    if (dev->volList[0]==NULL) {
-        free(dev->volList);
+    dev->volList[ 0 ] = adfVolCreate( dev, 0L, 80L, volName, volType );
+    if ( dev->volList[ 0 ] == NULL ) {
+        free( dev->volList );
         return ADF_RC_ERROR;
     }
-    dev->nVol = 1;
-    dev->devType = ( dev->sectors == 11 ) ? ADF_DEVTYPE_FLOPDD :
-                                            ADF_DEVTYPE_FLOPHD;
+    dev->nVol    = 1;
+    dev->devType = ( dev->sectors == 11 ? ADF_DEVTYPE_FLOPDD :
+                                          ADF_DEVTYPE_FLOPHD );
     dev->mounted = true;
 
     return ADF_RC_OK;
--- src/adf_dev_flop.h
+++ src/adf_dev_flop.h
@@ -6,9 +6,9 @@
 #include "adf_err.h"
 #include "adf_prefix.h"
 
-ADF_RETCODE adfMountFlop ( struct AdfDevice * dev );
+ADF_RETCODE adfMountFlop( struct AdfDevice *  dev );
 
-ADF_PREFIX ADF_RETCODE adfCreateFlop ( struct AdfDevice * const dev,
-                                       const char * const       volName,
-                                       const uint8_t            volType );
+ADF_PREFIX ADF_RETCODE adfCreateFlop( struct AdfDevice * const  dev,
+                                      const char * const        volName,
+                                      const uint8_t             volType );
 #endif  /* ADF_DEV_FLOP_H */
