a9e49d391bf71afdc29be6682a1a9e939f9ff3c5
t-m <tomas.wolak@gmail.com>
2025-03-13 17:03:43 +0100

    unadf tests: rename expected result files to match test names.

--- examples/tests/unadf-test.sh
+++ examples/tests/unadf-test.sh
@@ -7,7 +7,7 @@ UNADF=`get_test_cmd unadf`
 # check no args
 check_no_args() {
     $( $UNADF >$actual 2>&1 || exit 0; )
-    compare_with "check no args" unadf_1
+    compare_with "check no args" unadf_no_args
 }
 
 check_help() {
@@ -18,28 +18,30 @@ check_help() {
 # check invalid args
 check_invalid_args() {
     $( $UNADF -v >$actual 2>&1 || exit 0; )
-    compare_with "check invalid arg for -v" unadf_2
+    compare_with "check invalid arg for -v" unadf_invalid_args_1
 
     $( $UNADF -d >$actual 2>&1 || exit 0; )
-    compare_with "check invalid arg for -d" unadf_3
+    compare_with "check invalid arg for -d" unadf_invalid_args_2
 }
 
 # -l (list root directory) option
 check_list_root_directory_option() {
     $UNADF -l "$basedir/arccsh.adf" >$actual 2>/dev/null
-    compare_with "-l (list root directory) option" unadf_4
+    compare_with "-l (list root directory) option" \
+        unadf_list_root_directory_option
 }
 
 # -r (list entire disk) option
 check_list_entire_disk_option() {
     $UNADF -r "$basedir/arccsh.adf" >$actual 2>/dev/null
-    compare_with "-r (list entire disk) option" unadf_5
+    compare_with "-r (list entire disk) option" unadf_list_entire_disk_option
 }
 
 # -s (show logical block pointer) option
 check_show_logical_block_pointer_option() {
     $UNADF -ls "$basedir/arccsh.adf" >$actual 2>/dev/null
-    compare_with "-s (show logical block pointer) option" unadf_6
+    compare_with "-s (show logical block pointer) option" \
+        unadf_show_logical_block_pointer_option
 }
 # TODO -c (use dircache data) option
 # TODO -m (display file comments) option
@@ -48,7 +50,7 @@ check_show_logical_block_pointer_option() {
 # -d (extract to dir)
 check_extract_to_dir() {
     $UNADF -d $tmpdir/x "$basedir/arccsh.adf" >$actual 2>/dev/null
-    compare_with "-d (extract to dir)" unadf_7
+    compare_with "-d (extract to dir)" unadf_extract_to_dir
 
 # check permissions were set on extracted files
 # (these tests are failing on MSYS2:
@@ -68,26 +70,28 @@ check_extract_to_dir_with_specific_files() {
     $UNADF -d $tmpdir/x "$basedir/arccsh.adf" csh s/startup-sequence \
         devs/system-configuration >$actual 2>/dev/null
     compare_with "-d (extract to dir) with specific files, not all their original case" \
-        unadf_8
+        unadf_extract_to_dir_with_specific_files
 }
 # TODO check permissions were set (bug: currently they aren't)
 
 # -p (extract to pipe) option
 check_extract_to_pipe_option() {
     $UNADF -p "$basedir/arccsh.adf" s/startup-sequence >$actual 2>/dev/null
-    compare_with " -p (extract to pipe) option" unadf_9
+    compare_with " -p (extract to pipe) option" unadf_extract_to_pipe_option
 }
 
 # -w (mangle win32 filenames) option
 check_mangle_win32_filenames_option() {
     $UNADF -d $tmpdir/x -w "$basedir/win32-names.adf" >$actual 2>/dev/null
-    compare_with "-w (mangle win32 filenames) option" unadf_10
+    compare_with "-w (mangle win32 filenames) option" \
+        unadf_mangle_win32_filenames_option
 }
 
 # confirm the mangling (-w) only occurs on extraction
 check_confirm_the_mangling() {
     $UNADF -r -w "$basedir/win32-names.adf" >$actual 2>/dev/null
-    compare_with "confirm the mangling (-w) only occurs on extraction" unadf_11
+    compare_with "confirm the mangling (-w) only occurs on extraction" \
+        unadf_confirm_the_mangling
 }
 
 
