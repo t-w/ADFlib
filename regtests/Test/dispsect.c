c45691c838e9304dd2bef5ea2e9a1dcf00329460
t-m <tomas.wolak@gmail.com>
2025-03-16 20:13:56 +0100

    regtests / dispsect: use common logger.

--- regtests/Test/dispsect.c
+++ regtests/Test/dispsect.c
@@ -9,6 +9,9 @@
 
 #include"adflib.h"
 #include "common.h"
+#include "log.h"
+
+#define TEST_VERBOSITY 3
 
 
 void MyVer(char *msg)
@@ -24,6 +27,9 @@ void MyVer(char *msg)
 int main(int argc, char *argv[])
 {
     (void) argc, (void) argv;
+
+    log_init( stderr, TEST_VERBOSITY );
+
     struct AdfDevice *hd;
     struct AdfVolume *vol;
     struct AdfFile *fic;
@@ -41,7 +47,7 @@ int main(int argc, char *argv[])
     /* create and mount one device */
     hd = adfDevCreate ( "dump", "dispsect-newdev", 80, 2, 11 );
     if (!hd) {
-        fprintf(stderr, "can't mount device\n");
+        log_error( "can't create device\n" );
         adfEnvCleanUp(); exit(1);
     }
 
@@ -50,7 +56,7 @@ int main(int argc, char *argv[])
     if ( adfCreateFlop ( hd, "empty", ADF_DOSFS_FFS |
                                       ADF_DOSFS_DIRCACHE ) != ADF_RC_OK )
     {
-		fprintf(stderr, "can't create floppy\n");
+        log_error("can't create floppy\n");
         adfDevUnMount ( hd );
         adfDevClose ( hd );
         adfEnvCleanUp(); exit(1);
@@ -58,14 +64,16 @@ int main(int argc, char *argv[])
 
     vol = adfVolMount ( hd, 0, ADF_ACCESS_MODE_READWRITE );
     if (!vol) {
+        log_error( "can't mount volume\n" );
         adfDevUnMount ( hd );
         adfDevClose ( hd );
-        fprintf(stderr, "can't mount volume\n");
         adfEnvCleanUp(); exit(1);
     }
 
+    log_info( "\ncreate file_1a" );
     fic = adfFileOpen ( vol, "file_1a", ADF_FILE_MODE_WRITE );
     if (!fic) {
+        log_error( "can't open file\n" );
         adfVolUnMount(vol);
         adfDevUnMount ( hd );
         adfDevClose ( hd );
@@ -75,21 +83,20 @@ int main(int argc, char *argv[])
     adfFileWrite ( fic, 1, buf );
     adfFileClose ( fic );
 
-    puts("\ncreate file_1a");
     showVolInfo( vol );
 
+    log_info( "\ncreate dir_5u" );
     adfCreateDir(vol,vol->curDirPtr,"dir_5u");
-    puts("\ncreate dir_5u");
     showVolInfo( vol );
 
     showDirEntries( vol, vol->curDirPtr );
 
-    puts("\nremove file_1a");
+    log_info( "\nremove file_1a" );
     adfRemoveEntry(vol,vol->curDirPtr,"file_1a");
     showVolInfo( vol );
 
+    log_info( "\nremove dir_5u" );
     adfRemoveEntry(vol,vol->curDirPtr,"dir_5u");
-    puts("\nremove dir_5u");
     showVolInfo( vol );
 
     cell = list = adfGetDelEnt(vol);
@@ -104,12 +111,12 @@ int main(int argc, char *argv[])
     }
     adfFreeDelList(list);
 
+    log_info( "\nundel file_1a" );
     adfUndelEntry(vol,vol->curDirPtr,883); // file_1a
-    puts("\nundel file_1a");
     showVolInfo( vol );
 
+    log_info("\nundel dir_5u");
     adfUndelEntry(vol,vol->curDirPtr,885); // dir_5u
-    puts("\nundel dir_5u");
     showVolInfo( vol );
 
     showDirEntries( vol, vol->curDirPtr );
