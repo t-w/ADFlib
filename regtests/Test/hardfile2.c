5b8e0f88ecfb6abf79053406364c2eda0eb8a91d
t-m <tomas.wolak@gmail.com>
2025-03-17 15:38:13 +0100

    regtests / hardfile2: whitespaces (no semantic changes).

--- regtests/Test/hardfile2.c
+++ regtests/Test/hardfile2.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include"adflib.h"
+#include "adflib.h"
 #include "common.h"
 #include "log.h"
 
@@ -37,16 +37,16 @@ int main(int argc, char *argv[])
     int status = 0;
 
     /* create and mount one device : 4194304 bytes */
-    struct AdfDevice * const hd = adfDevCreate ( "dump", "hardfile2-newdev",
-                                                 256, 2, 32 );
-    if (!hd) {
+    struct AdfDevice * const hd = adfDevCreate( "dump", "hardfile2-newdev",
+                                                256, 2, 32 );
+    if ( ! hd ) {
         log_error( "can't create device\n" );
         status = 1;
         goto cleanup_env;
     }
 
-    if ( adfCreateHdFile ( hd, "empty", ADF_DOSFS_FFS |
-                                        ADF_DOSFS_DIRCACHE ) != ADF_RC_OK ) {
+    if ( adfCreateHdFile( hd, "empty", ADF_DOSFS_FFS |
+                                       ADF_DOSFS_DIRCACHE ) != ADF_RC_OK ) {
         log_error( "can't create hdfile (format)\n" );
         status = 1;
         goto cleanup_dev;
@@ -54,8 +54,8 @@ int main(int argc, char *argv[])
 
     showDevInfo( hd );
 
-    vol = adfVolMount ( hd, 0, ADF_ACCESS_MODE_READWRITE );
-    if (!vol) {
+    vol = adfVolMount( hd, 0, ADF_ACCESS_MODE_READWRITE );
+    if ( ! vol ) {
         log_error( "can't mount volume\n" );
         status = 1;
         goto cleanup_dev;
@@ -64,11 +64,11 @@ int main(int argc, char *argv[])
     showDirEntries( vol, vol->curDirPtr );
 
     /* unmounts */
-    adfVolUnMount(vol);
+    adfVolUnMount( vol );
 
 cleanup_dev:
-    adfDevUnMount ( hd );
-    adfDevClose ( hd );
+    adfDevUnMount( hd );
+    adfDevClose( hd );
 
 cleanup_env:
     adfEnvCleanUp();
