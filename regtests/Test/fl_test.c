b9ce5445dd1ef128eb6672fe5f40414101b00471
t-m <tomas.wolak@gmail.com>
2025-03-17 09:43:55 +0100

    regtests / fl_test: use common logger.

--- regtests/Test/fl_test.c
+++ regtests/Test/fl_test.c
@@ -9,6 +9,9 @@
 
 #include"adflib.h"
 #include "common.h"
+#include "log.h"
+
+#define TEST_VERBOSITY 3
 
 
 void MyVer(char *msg)
@@ -24,6 +27,9 @@ void MyVer(char *msg)
 int main(int argc, char *argv[])
 {
     (void) argc, (void) argv;
+
+    log_init( stderr, TEST_VERBOSITY );
+
     struct AdfVolume *vol;
     FILE* boot;
     unsigned char bootcode[1024];
@@ -35,24 +41,23 @@ int main(int argc, char *argv[])
     /* open and mount existing device */
     struct AdfDevice * hd = adfDevOpen ( argv[1], ADF_ACCESS_MODE_READWRITE );
     if ( ! hd ) {
-        fprintf ( stderr, "Cannot open file/device '%s' - aborting...\n",
-                  argv[1] );
+        log_error( "Cannot open file/device '%s' - aborting...\n", argv[1] );
         adfEnvCleanUp();
         exit(1);
     }
 
     ADF_RETCODE rc = adfDevMount ( hd );
     if ( rc != ADF_RC_OK ) {
-        fprintf(stderr, "can't mount device\n");
+        log_error( "can't mount device\n" );
         adfDevClose(hd);
         adfEnvCleanUp(); exit(1);
     }
 
     vol = adfVolMount ( hd, 0, ADF_ACCESS_MODE_READWRITE );
     if (!vol) {
+        log_error( "can't mount volume\n" );
         adfDevUnMount ( hd );
         adfDevClose ( hd );
-        fprintf(stderr, "can't mount volume\n");
         adfEnvCleanUp(); exit(1);
     }
 
@@ -62,12 +67,12 @@ int main(int argc, char *argv[])
     adfDevUnMount ( hd );
     adfDevClose ( hd );
 
-    putchar('\n');
+    log_info("\n");
 
     /* create and mount one device */
     hd = adfDevCreate ( "dump", "fl_test-newdev", 80, 2, 11 );
     if (!hd) {
-        fprintf(stderr, "can't mount device\n");
+        log_error( "can't create device\n" );
         adfEnvCleanUp(); exit(1);
     }
 
@@ -78,17 +83,17 @@ int main(int argc, char *argv[])
 
     vol = adfVolMount ( hd, 0, ADF_ACCESS_MODE_READWRITE );
     if (!vol) {
+        log_error( "can't mount volume\n" );
         adfDevUnMount ( hd );
         adfDevClose ( hd );
-        fprintf(stderr, "can't mount volume\n");
         adfEnvCleanUp(); exit(1);
     }
 
     boot=fopen(argv[2],"rb");
     if (!boot) {
+        log_error( "can't open bootblock file\n" );
         adfDevUnMount ( hd );
         adfDevClose ( hd );
-        fprintf(stderr, "can't mount volume\n");
         adfEnvCleanUp(); exit(1);
     }
     fread(bootcode, sizeof(unsigned char), 1024, boot);
