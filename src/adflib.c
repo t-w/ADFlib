60432ea5b9d69f9a544ccb14111420878a713116
t-m <tomas.wolak@gmail.com>
2025-03-20 13:01:01 +0100

    adfLibInit: let client code decide what to do on error.
    So far, the internal check was doing exit(1), now the function
    just returns an error and the client code must decide what next.

--- src/adflib.c
+++ src/adflib.c
@@ -34,7 +34,7 @@
 
 #include <stdlib.h>
 
-static void checkInternals(void);
+static unsigned checkInternals(void);
 
 
 char * adfGetVersionNumber(void) {
@@ -48,10 +48,8 @@ char * adfGetVersionDate(void) {
 
 ADF_RETCODE adfLibInit(void)
 {
-    //ADF_RETCODE status = checkInternals();
-    //if ( status != ADF_RC_OK )
-    //    return status;
-    checkInternals();
+    if ( checkInternals() > 0 )
+        return ADF_RC_ERROR_INTERNAL;
 
     adfEnvInitDefault();
     adfAddDeviceDriver( &adfDeviceDriverDump );
@@ -71,16 +69,18 @@ void adfLibCleanUp(void)
  * Internal checks
  ****************************************************************************/
 
-static void assertInternal( bool                cnd,
-                            const char * const  msg )
+static unsigned assertInternal( bool                cnd,
+                                const char * const  msg )
 {
     if ( ! cnd ) {
         fputs( msg, stderr );
-        exit( 1 );
-    }
+        //exit( 1 );
+        return 1;
+    } else
+        return 0;
 }
 
-static void checkInternals(void)
+static unsigned checkInternals(void)
 {
 /*    char str[80];*/
     union u {
@@ -88,51 +88,54 @@ static void checkInternals(void)
         char    c[ 4 ];
     } val;
 
-    /* internal checking */
+    unsigned nerrors =
+        assertInternal( sizeof(short) == 2,
+                        "Compilation error : sizeof(short) != 2\n" ) +
 
-    assertInternal( sizeof(short) == 2,
-                    "Compilation error : sizeof(short) != 2\n" );
+        assertInternal( sizeof(int32_t) == 4,
+                        "Compilation error : sizeof(int32_t) != 4\n" ) +
 
-    assertInternal( sizeof(int32_t) == 4,
-                    "Compilation error : sizeof(int32_t) != 4\n" );
+        assertInternal( sizeof(struct AdfEntryBlock) == 512,
+                        "Internal error : sizeof(struct AdfEntryBlock) != 512\n") +
 
-    assertInternal( sizeof(struct AdfEntryBlock) == 512,
-                    "Internal error : sizeof(struct AdfEntryBlock) != 512\n");
+        assertInternal( sizeof(struct AdfRootBlock) == 512,
+                        "Internal error : sizeof(struct AdfRootBlock) != 512\n") +
 
-    assertInternal( sizeof(struct AdfRootBlock) == 512,
-                    "Internal error : sizeof(struct AdfRootBlock) != 512\n");
+        assertInternal( sizeof(struct AdfDirBlock) == 512,
+                        "Internal error : sizeof(struct AdfDirBlock) != 512\n") +
 
-    assertInternal( sizeof(struct AdfDirBlock) == 512,
-                    "Internal error : sizeof(struct AdfDirBlock) != 512\n");
+        assertInternal( sizeof(struct AdfBootBlock) == 1024,
+                        "Internal error : sizeof(struct AdfBootBlock) != 1024\n" ) +
 
-    assertInternal( sizeof(struct AdfBootBlock) == 1024,
-                    "Internal error : sizeof(struct AdfBootBlock) != 1024\n" );
+        assertInternal( sizeof(struct AdfFileHeaderBlock) == 512,
+                        "Internal error : sizeof(struct AdfFileHeaderBlock) != 512\n" ) +
 
-    assertInternal( sizeof(struct AdfFileHeaderBlock) == 512,
-                    "Internal error : sizeof(struct AdfFileHeaderBlock) != 512\n" );
+        assertInternal( sizeof(struct AdfFileExtBlock) == 512,
+                        "Internal error : sizeof(struct AdfFileExtBlock) != 512\n" ) +
 
-    assertInternal( sizeof(struct AdfFileExtBlock) == 512,
-                    "Internal error : sizeof(struct AdfFileExtBlock) != 512\n" );
+        assertInternal( sizeof(struct AdfOFSDataBlock) == 512,
+                        "Internal error : sizeof(struct AdfOFSDataBlock) != 512\n" ) +
 
-    assertInternal( sizeof(struct AdfOFSDataBlock) == 512,
-                    "Internal error : sizeof(struct AdfOFSDataBlock) != 512\n" );
+        assertInternal( sizeof(struct AdfBitmapBlock) == 512,
+                        "Internal error : sizeof(struct AdfBitmapBlock) != 512\n" ) +
 
-    assertInternal( sizeof(struct AdfBitmapBlock) == 512,
-                    "Internal error : sizeof(struct AdfBitmapBlock) != 512\n" );
+        assertInternal( sizeof(struct AdfBitmapExtBlock) == 512,
+                        "Internal error : sizeof(struct AdfBitmapExtBlock) != 512\n" ) +
 
-    assertInternal( sizeof(struct AdfBitmapExtBlock) == 512,
-                    "Internal error : sizeof(struct AdfBitmapExtBlock) != 512\n" );
-
-    assertInternal( sizeof(struct AdfLinkBlock) == 512,
-                    "Internal error : sizeof(struct AdfLinkBlock) != 512\n" );
+        assertInternal( sizeof(struct AdfLinkBlock) == 512,
+                        "Internal error : sizeof(struct AdfLinkBlock) != 512\n" );
 
     val.l = 1L;
+
+    nerrors +=
 /* if LITT_ENDIAN not defined : must be BIG endian */
 #ifndef LITT_ENDIAN
-    assertInternal( val.c[3] == 1, /* little endian : LITT_ENDIAN must be defined ! */
-                    "Compilation error : LITT_ENDIAN not defined\n" );
+        assertInternal( val.c[3] == 1, /* little endian : LITT_ENDIAN must be defined ! */
+                        "Compilation error : LITT_ENDIAN not defined\n" );
 #else
-    assertInternal( val.c[3] != 1, /* big endian : LITT_ENDIAN must not be defined ! */
-                    "Compilation error : LITT_ENDIAN defined\n" );
+        assertInternal( val.c[3] != 1, /* big endian : LITT_ENDIAN must not be defined ! */
+                        "Compilation error : LITT_ENDIAN defined\n" );
 #endif
+
+    return nerrors;
 }
