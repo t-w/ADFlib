277400d4db3a04b26966ee56792b751761639ccd
t-m <tomas.wolak@gmail.com>
2025-03-26 12:00:56 +0100

    examples / tests: update test info (adf_show_metadata was renamed to adfinfo).

--- examples/tests/adf-floppy-test.sh
+++ examples/tests/adf-floppy-test.sh
@@ -13,11 +13,11 @@ $ADFFORMAT -t 1 -l TestFlopDD1 -v $tmpdir/testflopdd1.adf >$actual 2>/dev/null
 compare_with "adf format dd" adf-floppy-test_2
 
 $ADFINFO $tmpdir/testflopdd1.adf >$actual
-compare_with "adf show metadata dd floppy dev" adf-floppy-test_3
+compare_with "adfinfo dd floppy dev" adf-floppy-test_3
 
 $ADFINFO $tmpdir/testflopdd1.adf 0 | grep -v \
   -e Created: -e 'Last access:' -e checkSum: -e calculated -e days: \
   -e mins: -e ticks: -e coDays: -e coMins -e coTicks >$actual
-compare_with "adf show metadata dd floppy volume" adf-floppy-test_4
+compare_with "adfinfo dd floppy volume" adf-floppy-test_4
 
 read status < $status && test "x$status" = xsuccess
--- examples/tests/adfinfo-test.sh
+++ examples/tests/adfinfo-test.sh
@@ -5,18 +5,18 @@ basedir=`dirname "$0"`
 ADFINFO=`get_test_cmd adfinfo`
 
 $ADFINFO "$basedir/arccsh.adf" >$actual
-compare_with "adf show metadata device" adfinfo-test_1
+compare_with "adfinfo device" adfinfo-test_1
 
 $ADFINFO "$basedir/arccsh.adf" 0 >$actual
-compare_with "adf show metadata volume 0" adfinfo-test_2
+compare_with "adfinfo volume 0" adfinfo-test_2
 
 $ADFINFO "$basedir/arccsh.adf" 0 CSH >$actual
-compare_with "adf show metadata file CHS" adfinfo-test_3
+compare_with "adfinfo file CHS" adfinfo-test_3
 
 $ADFINFO "$basedir/arccsh.adf" 0 c/ >$actual
-compare_with "adf show metadata directory c/" adfinfo-test_4
+compare_with "adfinfo directory c/" adfinfo-test_4
 
 $ADFINFO "$basedir/arccsh.adf" 0 l >$actual
-compare_with "adf show metadata directory l" adfinfo-test_5
+compare_with "adfinfo directory l" adfinfo-test_5
 
 read status < $status && test "x$status" = xsuccess
