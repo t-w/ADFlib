c9afea58d38e1a09c3602c395e03167533f7f94b
t-m <tomas.wolak@gmail.com>
2025-03-28 12:01:47 +0100

    adf_dev_driver_nativ / generic: add new functions and update api.
    Should have been done in 4cb4e6ce (missed this one...).

--- src/generic/adf_dev_driver_nativ.c
+++ src/generic/adf_dev_driver_nativ.c
@@ -43,16 +43,42 @@ static struct AdfDevice * myInitDevice( const char * const   name,
 
 
 /*
+ * myReadSectors
+ *
+ */
+ADF_RETCODE myReadSectors( const struct AdfDevice * const  dev,
+                           const uint32_t                  block,
+                           const uint32_t                  lenBlocks,
+                           uint8_t * const                 buf )
+{
+    (void) dev, (void) block, (void) lenBlocks, (void) buf;
+    return ADF_RC_ERROR;
+}
+
+
+/*
+ * myWriteSectors
+ *
+ */
+ADF_RETCODE myWriteSectors( const struct AdfDevice * const  dev,
+                            const uint32_t                  block,
+                            const uint32_t                  lenBlocks,
+                            const uint8_t * const           buf )
+{
+    (void) dev, (void) block, (void) lenBlocks, (void) buf;
+    return ADF_RC_ERROR;
+}
+
+
+/*
  * myReadSector
  *
  */
 ADF_RETCODE myReadSector( const struct AdfDevice * const  dev,
                           const uint32_t                  n,
-                          const unsigned                  size,
                           uint8_t * const                 buf )
 {
-    (void) dev, (void) n, (void) size, (void) buf;
-    return ADF_RC_ERROR;
+    return myReadSectors( dev, n, 1, buf );
 }
 
 
@@ -62,11 +88,9 @@ ADF_RETCODE myReadSector( const struct AdfDevice * const  dev,
  */
 ADF_RETCODE myWriteSector( const struct AdfDevice * const  dev,
                            const uint32_t                  n,
-                           const unsigned                  size,
                            const uint8_t * const           buf )
 {
-    (void) dev, (void) n, (void) size, (void) buf;
-    return ADF_RC_ERROR;
+    return myWriteSectors( dev, n, 1, buf );
 }
 
 
@@ -100,15 +124,17 @@ static bool myIsDevice( const char * const  devName )
 
 
 const struct AdfDeviceDriver  adfDeviceDriverNative = {
-    .name        = "native generic",
-    .data        = NULL,
-    .createDev   = NULL,
-    .openDev     = myInitDevice,
-    .closeDev    = myReleaseDevice,
-    .readSector  = myReadSector,
-    .writeSector = myWriteSector,
-    .isNative    = myIsDevNative,
-    .isDevice    = myIsDevice
+    .name         = "native generic",
+    .data         = NULL,
+    .createDev    = NULL,
+    .openDev      = myInitDevice,
+    .closeDev     = myReleaseDevice,
+    .readSectors  = myReadSectors,
+    .writeSectors = myWriteSectors,
+    .readSector   = myReadSector,
+    .writeSector  = myWriteSector,
+    .isNative     = myIsDevNative,
+    .isDevice     = myIsDevice
 };
 
 
