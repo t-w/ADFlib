92b7f0c649edf0a38ba14fa90b24ffa866629ed8
t-m <tomas.wolak@gmail.com>
2025-04-01 19:07:08 +0200

    tests / examples / test_adf_floppy: minor refactoring.

--- tests/examples/test_adf_floppy.sh
+++ tests/examples/test_adf_floppy.sh
@@ -6,16 +6,18 @@ adfimgcreate=$(get_test_cmd adfimgcreate)
 adfformat=$(get_test_cmd adfformat)
 adfinfo=$(get_test_cmd adfinfo)
 
-$adfimgcreate -t dd $tmpdir/testflopdd1.adf >$actual
+dumpfile=$tmpdir/testflopdd1.adf
+
+$adfimgcreate -t dd $dumpfile >$actual
 compare_with "adfimgcreate -t dd" adf_floppy_test_1
 
-$adfformat -t 1 -l TestFlopDD1 -v $tmpdir/testflopdd1.adf >$actual 2>/dev/null
+$adfformat -t 1 -l TestFlopDD1 -v $dumpfile >$actual 2>/dev/null
 compare_with "adf format dd" adf_floppy_test_2
 
-$adfinfo $tmpdir/testflopdd1.adf >$actual
+$adfinfo $dumpfile >$actual
 compare_with "adfinfo dd floppy dev" adf_floppy_test_3
 
-$adfinfo $tmpdir/testflopdd1.adf 0 | grep -v \
+$adfinfo $dumpfile 0 | grep -v \
   -e Created: -e 'Last access:' -e checkSum: -e calculated -e days: \
   -e mins: -e ticks: -e coDays: -e coMins -e coTicks >$actual
 compare_with "adfinfo dd floppy volume" adf_floppy_test_4
