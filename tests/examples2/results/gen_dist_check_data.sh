0c543ed9bf86c33ae45232e3d619df8b4b699b22
t-m <tomas.wolak@gmail.com>
2025-04-06 18:54:20 +0200

    tests / examples2: (re)implement unadf tests in the new test framework.
    The test are split in 3 parts, depenging on which test dump file is used.
    
    Also, the generation in dist_check_DATA has been split, the results
    directory has its own Makefile.am.

--- tests/examples2/results/gen_dist_check_data.sh
+++ tests/examples2/results/gen_dist_check_data.sh
@@ -5,16 +5,13 @@
 
 set -e
 
-output=Makefile.am_cmd
+output=Makefile.am
 
-echo 'dist_check_DATA += \' >$output
+echo 'dist_check_DATA = \' >$output
 
-find cmd/ -type f | sort \
+find . -type f | grep -v -e Makefile.am -e gen_dist_check_data.sh \
+    | sort \
     | sed -e 's/^/\t/' -e 's/$/\ \\/' \
     >>$output
 
 truncate -s -2 $output
-
-cd results
-./gen_dist_check_data.sh
-cd ..
