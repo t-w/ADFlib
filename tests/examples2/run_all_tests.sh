ec9a1a0cd9268eaf07ac8322a525721d5843ba86
t-m <tomas.wolak@gmail.com>
2025-04-07 15:14:47 +0200

    tests / examples2: add a script for running all tests.

---
+++ tests/examples2/run_all_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Enable verbose output if VERBOSE=1
+[ "$VERBOSE" = "1" ] && set -x
+
+set -e
+set -x
+
+baseDir=$(dirname $0)
+
+tests[0]="test_adfbitmap.sh"
+tests[1]="test_adf_floppy.sh"
+tests[2]="test_adfinfo_hd.sh"
+tests[3]="test_adfinfo.sh"
+tests[4]="test_adfsalvage.sh"
+tests[5]="test_unadf_hdd.sh"
+tests[6]="test_unadf.sh"
+tests[7]="test_unadf_win32.sh"
+
+for i in "${tests[@]}"
+do
+    ${baseDir}/${i}
+done
