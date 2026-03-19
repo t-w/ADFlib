4605e6e04d444c18a41ba8a1fc9e9d2bbbbce706
t-m <tomas.wolak@gmail.com>
2026-03-19 23:20:58 +0100

    tests / test_util: minor changes (var. names, whitespaces).
    Also, make const things const.

--- tests/unit/test_util.c
+++ tests/unit/test_util.c
@@ -6,45 +6,45 @@
 #include "test_util.h"
 #include "adf_file_util.h"
 
-unsigned verify_file_data ( struct AdfVolume * const    vol,
-                            const char * const          filename,
-                            const unsigned char * const buffer,
-                            const unsigned              fsize,
-                            const unsigned              errors_max )
+unsigned verify_file_data( struct AdfVolume * const    vol,
+                           const char * const          filename,
+                           const unsigned char * const buffer,
+                           const unsigned              fsize,
+                           const unsigned              errors_max )
 {
-    struct AdfFile * const output = adfFileOpen ( vol, filename, ADF_FILE_MODE_READ );
+    struct AdfFile * const output = adfFileOpen( vol, filename, ADF_FILE_MODE_READ );
     if ( ! output )
         return 1;
 
-#ifdef _MSC_VER   // visual studio do not understand that const is const...
+#ifdef _MSC_VER   // visual studio does not allow use const as array size
 #define READ_BUFSIZE 1024
 #else
     const unsigned READ_BUFSIZE = 1024;
 #endif
-    unsigned char readbuf [ READ_BUFSIZE ];
+    unsigned char readbuf[ READ_BUFSIZE ];
 
-    unsigned bytes_read = 0,
+    unsigned bytes_read    = 0,
              bytes_to_read = fsize,
-             offset = 0,
-             nerrors = 0,
+             offset        = 0,
+             nerrors       = 0,
              chunk_bytes_to_read,
              chunk_bytes_read;
     while ( bytes_to_read > 0 ) {
         chunk_bytes_to_read = ( READ_BUFSIZE < bytes_to_read ? READ_BUFSIZE : bytes_to_read );
-        chunk_bytes_read = (unsigned) adfFileRead ( output, chunk_bytes_to_read, readbuf );
+        chunk_bytes_read    = (unsigned) adfFileRead( output, chunk_bytes_to_read, readbuf );
         bytes_read    += chunk_bytes_read;
         bytes_to_read -= chunk_bytes_read;
 
-        for ( unsigned i = 0 ; i < chunk_bytes_read ; ++i ) {
-            if ( readbuf [ offset % READ_BUFSIZE ] != buffer [ offset ] ) {
-                fprintf ( stderr, "Data differ at %u ( 0x%x ): orig. 0x%x, read 0x%x\n",
-                          offset, offset,
-                          buffer [ offset ],
-                          readbuf [ offset % READ_BUFSIZE ] );
-                fflush ( stderr );
+        for ( unsigned i = 0; i < chunk_bytes_read; ++i ) {
+            if ( readbuf[ offset % READ_BUFSIZE ] != buffer[ offset ] ) {
+                fprintf( stderr, "Data differ at %u ( 0x%x ): orig. 0x%x, read 0x%x\n",
+                         offset, offset,
+                         buffer[ offset ],
+                         readbuf[ offset % READ_BUFSIZE ] );
+                fflush( stderr );
                 nerrors++;
                 if ( nerrors > errors_max ) {
-                    adfFileClose ( output );
+                    adfFileClose( output );
                     return nerrors;
                 }
             }
@@ -52,12 +52,12 @@ unsigned verify_file_data ( struct AdfVolume * const    vol,
         }
     }
 
-    adfFileClose ( output );
+    adfFileClose( output );
 
     if ( bytes_read != fsize ) {
-        fprintf ( stderr, "bytes read (%u) != bytes written (%u) -> ERROR!!!\n",
-                  bytes_read, fsize );
-        fflush ( stderr );
+        fprintf( stderr, "bytes read (%u) != bytes written (%u) -> ERROR!!!\n",
+                 bytes_read, fsize );
+        fflush( stderr );
         nerrors++;
     }
 
@@ -65,21 +65,21 @@ unsigned verify_file_data ( struct AdfVolume * const    vol,
 }
 
 
-static unsigned validate_file_metadata_last_ext ( struct AdfFile * const file )
+static unsigned validate_file_metadata_last_ext( struct AdfFile * const file )
 {
-    unsigned fsize = file->fileHdr->byteSize;
+    const unsigned fsize = file->fileHdr->byteSize;
 
-    ADF_RETCODE rc = adfFileSeek ( file, fsize + 1 );
-    if ( rc != ADF_RC_OK || ! adfFileAtEOF ( file ) ) {
+    const ADF_RETCODE rc = adfFileSeek( file, fsize + 1 );
+    if ( rc != ADF_RC_OK || ! adfFileAtEOF( file ) ) {
         return 1;
     }
     //unsigned nerrors = 0;
-    unsigned nDataBlocks = adfFileSize2Datablocks ( file->fileHdr->byteSize,
-                                                    file->volume->datablockSize );
-    unsigned nExtBlocks  = adfFileDatablocks2Extblocks ( nDataBlocks );
-    //printf ("nDataBlocks %u, nExtBlocks %u, bufsize %u, truncsize %u\n",
+    const unsigned nDataBlocks = adfFileSize2Datablocks( file->fileHdr->byteSize,
+                                                         file->volume->datablockSize );
+    const unsigned nExtBlocks  = adfFileDatablocks2Extblocks( nDataBlocks );
+    //printf( "nDataBlocks %u, nExtBlocks %u, bufsize %u, truncsize %u\n",
     //        nDataBlocks, nExtBlocks, bufsize, truncsize );
-    //fflush(stdout);
+    //fflush( stdout );
 
     // make sure we have available current ext. block (if needed)
     //int32_t * const dataBlocks = ( nExtBlocks < 1 ) ? file->fileHdr->dataBlocks :
@@ -91,16 +91,16 @@ static unsigned validate_file_metadata_last_ext ( struct AdfFile * const file )
     } else {  // ( nExtBlocks >= 1 )
         if  ( file->volume->datablockSize != 488 ) {
             // FFS
-            //ck_assert_ptr_nonnull ( file->currentExt );
+            //ck_assert_ptr_nonnull( file->currentExt );
             if ( file->currentExt == NULL )
                 return 2;
             dataBlocks = file->currentExt->dataBlocks;
         } else {
             // for OFS - we must read the current ext.(!)
-            fext = (struct AdfFileExtBlock *) malloc ( sizeof (struct AdfFileExtBlock) );
+            fext = (struct AdfFileExtBlock *) malloc( sizeof( struct AdfFileExtBlock ) );
             if ( fext == NULL )
                 return 3;
-            if ( adfFileReadExtBlockN ( file, (int) nExtBlocks - 1, fext ) !=  ADF_RC_OK )
+            if ( adfFileReadExtBlockN( file, (int) nExtBlocks - 1, fext ) !=  ADF_RC_OK )
                 return 4;
             dataBlocks = fext->dataBlocks;
         }
@@ -108,11 +108,11 @@ static unsigned validate_file_metadata_last_ext ( struct AdfFile * const file )
 
     // check the number of non-zero blocks in the array of the last metadata block (header or ext)
     unsigned nonZeroCount = 0;
-    for ( unsigned i = 0 ; i < ADF_MAX_DATABLK ; ++i ) {
+    for ( unsigned i = 0; i < ADF_MAX_DATABLK; ++i ) {
         if ( dataBlocks[i] != 0 )
             nonZeroCount++;
     }
-    free(fext);
+    free( fext );
 
     if ( file->fileHdr->byteSize == 0 ) {
         //ck_assert_uint_eq ( nonZeroCount, 0 );
@@ -124,7 +124,7 @@ static unsigned validate_file_metadata_last_ext ( struct AdfFile * const file )
                                      ADF_MAX_DATABLK );
         //ck_assert_uint_eq ( nonZeroCount, nonZeroExpected );
         if ( nonZeroCount != nonZeroExpected ) {
-            printf ("Incorrect number of non-zero blocks in the last metadata block:"
+            printf( "Incorrect number of non-zero blocks in the last metadata block:"
                     "nonZeroCount %u != nonZeroExpected %u, filesize %u",
                     nonZeroCount, nonZeroExpected, file->fileHdr->byteSize );
             return 6;
@@ -134,59 +134,58 @@ static unsigned validate_file_metadata_last_ext ( struct AdfFile * const file )
 }
 
 
-unsigned validate_file_metadata ( struct AdfVolume * const vol,
-                                  const char * const       filename,
-                                  const unsigned           errors_max )
+unsigned validate_file_metadata( struct AdfVolume * const vol,
+                                 const char * const       filename,
+                                 const unsigned           errors_max )
 {
     (void) errors_max;
-    struct AdfFile * const file = adfFileOpen ( vol, filename, ADF_FILE_MODE_READ );
+    struct AdfFile * const file = adfFileOpen( vol, filename, ADF_FILE_MODE_READ );
     if ( ! file )
         return 1;
-    unsigned nerrors = validate_file_metadata_last_ext ( file );
-    adfFileClose ( file );
+    const unsigned nerrors = validate_file_metadata_last_ext( file );
+    adfFileClose( file );
     return nerrors;
 }
 
 
 // bufsize must be divisible by 4
-void pattern_AMIGAMIG ( unsigned char * buf,
-                        const unsigned  bufsize )
+void pattern_AMIGAMIG( unsigned char * const buf,
+                       const unsigned        bufsize )
 {
-    for ( unsigned i = 0 ; i < bufsize ; i += 4 ) {
-        buf[i]   = 'A';
-        buf[i+1] = 'M';
-        buf[i+2] = 'I';
-        buf[i+3] = 'G';
+    for ( unsigned i = 0; i < bufsize; i += 4 ) {
+        buf[ i ]     = 'A';
+        buf[ i + 1 ] = 'M';
+        buf[ i + 2 ] = 'I';
+        buf[ i + 3 ] = 'G';
      }
 }
 
 
-void pattern_random ( unsigned char * buf,
-                      const unsigned  bufsize )
+void pattern_random( unsigned char * const buf,
+                     const unsigned        bufsize )
 {
-    for ( unsigned i = 0 ; i < bufsize ; ++i ) {
-        buf[i] = (unsigned char) ( rand() & 0xff );
-    }
+    for ( unsigned i = 0; i < bufsize; ++i )
+        buf[ i ] = (unsigned char) ( rand() & 0xff );
 }
 
 
-unsigned pos2datablockIndex ( unsigned pos,
-                              unsigned blocksize )
+unsigned pos2datablockIndex( const unsigned pos,
+                             const unsigned blocksize )
 {
     return ( pos / blocksize );
 }
 
 
 
-unsigned filesize2datablocks ( unsigned fsize,
-                               unsigned blocksize )
+unsigned filesize2datablocks( const unsigned fsize,
+                              const unsigned blocksize )
 {
     return ( fsize / blocksize +
              ( ( fsize % blocksize > 0 ) ? 1 : 0 ) );
 }
 
 
-unsigned datablocks2extblocks ( unsigned data_blocks )
+unsigned datablocks2extblocks( const unsigned data_blocks )
 {
     //return max ( ( data_blocks - 1 ) / ADF_MAX_DATABLK, 0 );
     if ( data_blocks < 1 )
@@ -195,19 +194,19 @@ unsigned datablocks2extblocks ( unsigned data_blocks )
 }
 
 
-unsigned filesize2blocks ( unsigned fsize,
-                           unsigned blocksize )
+unsigned filesize2blocks( const unsigned fsize,
+                          const unsigned blocksize )
 {
     //assert ( fsize >= 0 );
     //assert ( blocksize >= 0 );
-    unsigned data_blocks = filesize2datablocks ( fsize, blocksize );
-    unsigned ext_blocks = datablocks2extblocks ( data_blocks );
+    const unsigned data_blocks = filesize2datablocks( fsize, blocksize );
+    const unsigned ext_blocks  = datablocks2extblocks( data_blocks );
     return data_blocks + ext_blocks + 1;   // +1 is the file header block
 }
 
 
 // datablock index ( 0, ...  , nblocks - 1 ) to block's position in ExtBlock
-unsigned datablock2posInExtBlk ( unsigned datablock_idx )
+unsigned datablock2posInExtBlk( const unsigned datablock_idx )
 {
     //return max ( ( datablock_idx - ADF_MAX_DATABLK ) % ( ADF_MAX_DATABLK + 1 ), 0 );
 
--- tests/unit/test_util.h
+++ tests/unit/test_util.h
@@ -1,38 +1,38 @@
 
-#ifndef __TEST_UTIL_H__
-#define __TEST_UTIL_H__
+#ifndef TEST_UTIL_H
+#define TEST_UTIL_H
 
 #include "adflib.h"
 
 
-unsigned verify_file_data ( struct AdfVolume * const    vol,
-                            const char * const          filename,
-                            const unsigned char * const buffer,
-                            const unsigned              bytes_written,
-                            const unsigned              errors_max );
+unsigned verify_file_data( struct AdfVolume * const    vol,
+                           const char * const          filename,
+                           const unsigned char * const buffer,
+                           const unsigned              bytes_written,
+                           const unsigned              errors_max );
 
-unsigned validate_file_metadata ( struct AdfVolume * const vol,
-                                  const char * const       filename,
-                                  const unsigned           errors_max );
+unsigned validate_file_metadata( struct AdfVolume * const vol,
+                                 const char * const       filename,
+                                 const unsigned           errors_max );
 
-void pattern_AMIGAMIG ( unsigned char * buf,
-                        const unsigned  BUFSIZE );
+void pattern_AMIGAMIG( unsigned char * const buf,
+                       const unsigned        bufsize );
 
-void pattern_random ( unsigned char * buf,
-                      const unsigned  BUFSIZE );
+void pattern_random( unsigned char * const buf,
+                     const unsigned        bufsize );
 
 
-unsigned pos2datablockIndex ( unsigned pos,
-                              unsigned blocksize );
+unsigned pos2datablockIndex( const unsigned pos,
+                             const unsigned blocksize );
 
-unsigned filesize2datablocks ( unsigned fsize,
-                               unsigned blocksize );
+unsigned filesize2datablocks( const unsigned fsize,
+                              const unsigned blocksize );
 
-unsigned datablocks2extblocks ( unsigned data_blocks );
+unsigned datablocks2extblocks( const unsigned data_blocks );
 
-unsigned filesize2blocks ( unsigned fsize,
-                           unsigned blocksize );
+unsigned filesize2blocks( const unsigned fsize,
+                          const unsigned blocksize );
 
-unsigned datablock2posInExtBlk ( unsigned datablock_idx );
+unsigned datablock2posInExtBlk( const unsigned datablock_idx );
 
 #endif
